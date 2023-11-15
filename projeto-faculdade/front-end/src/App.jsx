0d25e6cd17aa1fc271295aa02d876970b8bf331d
caioneves05 <ncaio037@gmail.com>
2023-11-15 16:45:14 -0300

    Adicionando a tela de login do projeto

--- projeto-faculdade/front-end/src/App.jsx
+++ projeto-faculdade/front-end/src/App.jsx
@@ -1,10 +1,13 @@
+
 import { MainContent } from "../src/templates/MainContent/";
 import { Login } from "./templates/Login";
 
 const App = () => {
   
   return (
+    <>
     <Login />
+    </>
   );
 };
 
--- projeto-faculdade/front-end/src/templates/Login/index.jsx
+++ projeto-faculdade/front-end/src/templates/Login/index.jsx
@@ -6,72 +6,58 @@ import {
     Input,
     Stack,
     Button,
-    Radio,
-    RadioGroup,
     Heading,
+    FormErrorMessage,
   } from '@chakra-ui/react';
+import { useState } from 'react';
+import { login } from '../../services/api';
   
   export const Login = () => {
+    const [emailValue, setEmailValue] = useState('')
+    const [passwordValue, setPasswordValue] = useState('')
+
+    const [errorValidation, setErrorValidation] = useState(false)
+
     return (
+      <>
       <Center h="100vh">
-        <Box p={10} maxW="900px" borderWidth="1px" borderRadius="md" padding={'16'} boxShadow="lg">
+        <Box p={10} maxW="900px" borderWidth="1px" borderRadius="md" padding={'16'} boxShadow="lg" backgroundColor={'#f4f4f4'}>
           <Heading as="h1" size="lg" mb={4}>
-            Cadastro de Usuário
+            Login
           </Heading>
           <form>
-            <Stack spacing={3}>
-              <FormControl isRequired>
-                <FormLabel htmlFor="nome">Nome:</FormLabel>
-                <Input type="text" id="nome" name="nome" />
-              </FormControl>
+            <Stack spacing={3} mt={4}>
   
-              <FormControl isRequired>
+              <FormControl isRequired isInvalid={errorValidation} >
                 <FormLabel htmlFor="email">E-mail:</FormLabel>
-                <Input type="email" id="email" name="email" />
+                <Input backgroundColor={'white'} onChange={(value) => setEmailValue(value.target.value)} type="email" id="email" name="email" />
+                {errorValidation && (
+                  <FormErrorMessage>Dados Incorretos</FormErrorMessage>
+                )}
               </FormControl>
   
-              <FormControl isRequired>
-                <FormLabel htmlFor="senha">Senha:</FormLabel>
-                <Input type="password" id="senha" name="senha" />
+              <FormControl isRequired isInvalid={errorValidation}>
+                <FormLabel>Senha:</FormLabel>
+                <Input backgroundColor={'white'} onChange={(value) => setPasswordValue(value.target.value)} type="password" id="senha" name="senha" />
+                {errorValidation && (
+                  <FormErrorMessage>Dados Incorretos</FormErrorMessage>
+                )}
               </FormControl>
   
-              <FormControl isRequired>
-                <FormLabel htmlFor="confirmar-senha">Confirmar Senha:</FormLabel>
-                <Input type="password" id="confirmar-senha" name="confirmar-senha" />
-              </FormControl>
-  
-              <FormControl>
-                <FormLabel>Gênero:</FormLabel>
-                <RadioGroup id="genero" name="genero">
-                  <Stack direction="row">
-                    <Radio value="Masculino">Masculino</Radio>
-                    <Radio value="Feminino">Feminino</Radio>
-                    <Radio value="Outro">Outro</Radio>
-                  </Stack>
-                </RadioGroup>
-              </FormControl>
-  
-              <FormControl isRequired>
-                <FormLabel htmlFor="data-nascimento">Data de Nascimento:</FormLabel>
-                <Input type="date" id="data-nascimento" name="data-nascimento" />
-              </FormControl>
-  
-              <FormControl>
-                <FormLabel htmlFor="cidade">Cidade:</FormLabel>
-                <Input type="text" id="cidade" name="cidade" />
-              </FormControl>
-  
-              <FormControl>
-                <FormLabel htmlFor="pais">País:</FormLabel>
-                <Input type="text" id="pais" name="pais" />
-              </FormControl>
-  
-              <Button type="submit" colorScheme="teal" w="100%">
-                Cadastrar
+              <Button onClick={async () => {
+                login({
+                  email: emailValue,
+                  password: passwordValue
+                }).then((response) => response).catch(() => {
+                  setErrorValidation(!errorValidation)
+                })
+              }} type="button" colorScheme="teal" w="100%">
+                Login
               </Button>
             </Stack>
           </form>
         </Box>
       </Center>
+      </>
     );
   };
